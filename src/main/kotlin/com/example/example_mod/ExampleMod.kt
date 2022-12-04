a051a50839efdc8289e50e442d94e01845fb5b31
NoComment1105 <nocomment1105@outlook.com>
2022-12-04 17:08:08 +0000

    Convert main class to a main object and remove false nullable

--- src/main/kotlin/com/example/example_mod/ExampleMod.kt
+++ src/main/kotlin/com/example/example_mod/ExampleMod.kt
@@ -5,12 +5,10 @@ import org.quiltmc.qsl.base.api.entrypoint.ModInitializer
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
-class ExampleMod : ModInitializer {
-    override fun onInitialize(mod: ModContainer?) {
-        LOGGER.info("Hello Quilt world from {}!", mod?.metadata()?.name())
-    }
+object ExampleMod : ModInitializer {
+    private val LOGGER: Logger = LoggerFactory.getLogger("Example Mod")
 
-    companion object {
-        val LOGGER: Logger = LoggerFactory.getLogger("Example Mod")
+    override fun onInitialize(mod: ModContainer) {
+        LOGGER.info("Hello Quilt world from {}!", mod.metadata()?.name())
     }
 }
