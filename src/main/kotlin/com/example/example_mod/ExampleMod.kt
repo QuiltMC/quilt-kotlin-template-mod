f17c3905374ebe3a37b4efa1aee4a50b17a3760c
Lilly Rosaline <lilly.rosaline@waterlilly.dev>
2022-12-09 00:15:01 -0600

    Revert "fix cause of IllegalAccessException"
    This reverts commit 1edbf0204daaa7f5f3d45d7649021e4d1952f93b.

--- src/main/kotlin/com/example/example_mod/ExampleMod.kt
+++ src/main/kotlin/com/example/example_mod/ExampleMod.kt
@@ -5,11 +5,10 @@ import org.quiltmc.qsl.base.api.entrypoint.ModInitializer
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
-class ExampleMod : ModInitializer {
+object ExampleMod : ModInitializer {
+    private val LOGGER: Logger = LoggerFactory.getLogger("Example Mod")
+
     override fun onInitialize(mod: ModContainer) {
         LOGGER.info("Hello Quilt world from {}!", mod.metadata()?.name())
-    }
-    companion object {
-        private val LOGGER: Logger = LoggerFactory.getLogger("Example Mod")
     }
 }
